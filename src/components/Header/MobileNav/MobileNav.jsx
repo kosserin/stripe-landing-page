74e188bd3089664532f7d31447fa1cdaa2175691
Andrija <kostic.andrija00@gmail.com>
2022-06-22 14:23:08 +0200

    navigation done

--- src/components/Header/MobileNav/MobileNav.jsx
+++ src/components/Header/MobileNav/MobileNav.jsx
@@ -12,7 +12,7 @@ const MobileNav = (props) => {
 
   return (
     <nav className={styles['mobile-nav']}>
-        <button onClick={closeButtonHandler}><FaTimes /></button>
+        <button className={styles['close-btn']} onClick={closeButtonHandler}><FaTimes /></button>
         <div className={styles['mobile-lists']}>
           <div className={styles['mobile-columns']}>
             <h3>Products</h3>
@@ -20,41 +20,268 @@ const MobileNav = (props) => {
               <li>
                 <a href="#">
                   <Blob />
-                  <div className={styles['products-list__text']}>
-                    <h4><span>Payments</span><FaChevronRight /></h4>
-                    <h5>Online payments</h5>
-                  </div>
+                  <h4>Online</h4>
                 </a>
               </li>
               <li>
                 <a href="#">
                   <Blob />
-                  <div className={styles['products-list__text']}>
-                    <h4><span>Payments</span><FaChevronRight /></h4>
-                    <h5>Online payments</h5>
-                  </div>
+                  <h4>Online</h4>
                 </a>
               </li>
               <li>
                 <a href="#">
                   <Blob />
-                  <div className={styles['products-list__text']}>
-                    <h4><span>Payments</span><FaChevronRight /></h4>
-                    <h5>Online payments</h5>
-                  </div>
+                  <h4>Online</h4>
                 </a>
               </li>
               <li>
                 <a href="#">
                   <Blob />
-                  <div className={styles['products-list__text']}>
-                    <h4><span>Payments</span><FaChevronRight /></h4>
-                    <h5>Online payments</h5>
-                  </div>
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Blob />
+                  <h4>Online</h4>
                 </a>
               </li>
             </ul>
           </div>
+          <div className={styles['mobile-columns']}>
+            <h3>Solutions</h3>
+            <ul>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles['mobile-columns']}>
+            <ul>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles['mobile-columns']}>
+          <h3>Resources</h3>
+            <ul>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <FaBookOpen />
+                  <h4>Ecommerce</h4>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles['mobile-button']}>
+          <button className={styles['sign-in__btn']}>Sign in <FaChevronRight /></button>
         </div>
       </nav>
   )
